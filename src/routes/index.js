6f412a5a25b726e97bce891fd6840463daf5fcfd
Jovan F <jovanfinesta@gmail.com>
2025-09-18 06:58:56 +0700

    inisialisasi routes list mahasiswa

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,4 +6,12 @@ router.get("/", (req, res) => {
   res.json({ message: "test" });
 });
 
+// List Mahasiswa (per halaman)
+router.get("/mahasiswa", async (req, res) => {
+  const {page, search} = req.query
+
+  res.json(req.query);
+  // res.json(getMahasiswa(page, search));
+});
+
 export default router;
