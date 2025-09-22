73ef1ed1d675e3e49efd04ebe8b8d3b85674e943
Fayyad Mohammad Madani <fayyadmadani.11@gmail.com>
2025-09-22 21:55:38 +0700

    feat: add mahasiswa by ID endpoint with swagger docs

--- src/routes/mahasiswabyid.js
+++ src/routes/mahasiswabyid.js
@@ -3,15 +3,6 @@ import { getStudentById } from "../lib/students.js";
 
 const router = Router();
 
-// List mahasiswa (default)
-router.get("/", async (req, res) => {
-  const { page, search } = req.query;
-
-  res.json(req.query);
-  // respons yg dibutuhkan: data2 mahasiswa sesuai page, jumlah page, ___
-  // res.json(getMahasiswa(page, search)); // Function untuk query dari database akan segera datang!!!
-});
-
 // Get mahasiswa by ID
 router.get("/:id", async (req, res) => {
   try {
