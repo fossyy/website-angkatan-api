487c7c6f65db85758a2643ad4c66f3be6128ef0d
Fayyad M Madani <fayyadmadani.11@gmail.com>
2025-10-27 00:34:11 +0700

    update: testing with mock data

--- src/lib/arunglink.js
+++ src/lib/arunglink.js
@@ -1,5 +1,33 @@
 import { pool } from "./db.js";
 
+const MOCK_MODE = true;
+const TEST_DATA = [
+  {
+    slug: 'scele-ui',
+    title: 'SCELE UI',
+    category: 'academic',
+    link: 'https://scele.ui.ac.id',
+    status: 'on',
+    uploaded_at: '2025-01-20T08:00:00.000Z'
+  },
+  {
+    slug: 'github-repo',
+    title: 'Github Repository',
+    category: 'administration',
+    link: 'https://github.com',
+    status: 'on',
+    uploaded_at: '2025-09-20T08:00:00.000Z'
+  },
+  {
+    slug: 'discord-link',
+    title: 'Discord Link',
+    category: 'event',
+    link: 'https://discord.com/',
+    status: 'none',
+    uploaded_at: '2024-01-20T08:00:00.000Z'
+  }
+];
+
 function slugify(text) {
   return text
     .toString()
@@ -40,6 +68,8 @@ export async function removeArungLinkBySlug(slug) {
 
 // Get all links
 export async function getAllLinks() {
+  if (MOCK_MODE) return TEST_DATA; // Uncomment untuk testing
+  
   const query = `
     SELECT slug, title, category, link, status, uploaded_at
     FROM arunglink
